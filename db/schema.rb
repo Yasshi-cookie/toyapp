c9778de4cff976071d34b0484be909961b730a4c
Yasushi Teruya <y.teruyacookie@gmail.com>
2020-04-15 15:23:37 +0900

    add character limit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200408064040) do
+ActiveRecord::Schema.define(version: 20200415055842) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
